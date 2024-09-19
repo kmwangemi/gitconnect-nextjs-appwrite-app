aa1190e8ad3f232293a97662730ac81892bd3b1c
kmwangemi <mwangemik@gmail.com>
2024-09-19 13:13:25 +0300

    feat: add auth forms & actions

---
+++ app/(auth)/login/actions.ts
@@ -0,0 +1,58 @@
+"use server";
+
+import { loginSchema, LoginValues } from "@/lib/validation";
+import { isRedirectError } from "next/dist/client/components/redirect";
+import { redirect } from "next/navigation";
+
+export async function login(
+  credentials: LoginValues,
+): Promise<{ error: string }> {
+  try {
+    const { username, password } = loginSchema.parse(credentials);
+    console.log(username, password);
+
+    // const existingUser = await prisma.user.findFirst({
+    //   where: {
+    //     username: {
+    //       equals: username,
+    //       mode: "insensitive",
+    //     },
+    //   },
+    // });
+
+    // if (!existingUser || !existingUser.passwordHash) {
+    //   return {
+    //     error: "Incorrect username or password",
+    //   };
+    // }
+
+    // const validPassword = await verify(existingUser.passwordHash, password, {
+    //   memoryCost: 19456,
+    //   timeCost: 2,
+    //   outputLen: 32,
+    //   parallelism: 1,
+    // });
+
+    // if (!validPassword) {
+    //   return {
+    //     error: "Incorrect username or password",
+    //   };
+    // }
+
+    // const session = await lucia.createSession(existingUser.id, {});
+    // const sessionCookie = lucia.createSessionCookie(session.id);
+    // cookies().set(
+    //   sessionCookie.name,
+    //   sessionCookie.value,
+    //   sessionCookie.attributes,
+    // );
+
+    return redirect("/");
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+    console.error(error);
+    return {
+      error: "Something went wrong. Please try again.",
+    };
+  }
+}
